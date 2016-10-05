bddb3c547c27c90f9bb6913445822ff4ae3dc2aa
KyloJorgensen <kylo.j.jorgensen@gmail.com>
2016-10-05 15:39:08 -0600

    changed call back to Herokuapp

--- server/config/variables.express.js
+++ server/config/variables.express.js
@@ -6,5 +6,5 @@ module.exports = {
     SPOTIFY_STATE_KEY: 'spotify_auth_state',
     SPOTIFY_CLIENT_ID: '52400559933b4fc6b0eacb80c934ab4c',
 	SPOTIFY_CLIENT_SECRET: 'ef65f6574b46456f9a6a54989207e8cd',
-	SPOTIFY_REDIRECT_URI: 'http://localhost:8080/callback/'
+	SPOTIFY_REDIRECT_URI: 'https://spotify-your-music-quiz.herokuapp.com/callback/'
 };
